151045256471b3938d268ce51f0492547c3159e4
aisha115 <ayushipatel1152@gmail.com>
2023-05-20 21:10:08 +0530

    #

--- collectimages.py
+++ collectimages.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np
 import os
 import string
-# Create the directory structure
-# if not os.path.exists("data"):
-#     os.makedirs("data")
-# if not os.path.exists("data/train"):
-#     os.makedirs("data/train")
-# if not os.path.exists("data/test"):
-#     os.makedirs("data/test")
+Create the directory structure
+if not os.path.exists("data"):
+    os.makedirs("data")
+if not os.path.exists("data/train"):
+    os.makedirs("data/train")
+if not os.path.exists("data/test"):
+    os.makedirs("data/test")
 
-# for i in string.ascii_uppercase:
-#     if not os.path.exists("data/train/" + i):
-#         os.makedirs("data/train/"+i)
-#     if not os.path.exists("data/test/" + i):
-#         os.makedirs("data/test/"+i)
+for i in string.ascii_uppercase:
+    if not os.path.exists("data/train/" + i):
+        os.makedirs("data/train/"+i)
+    if not os.path.exists("data/test/" + i):
+        os.makedirs("data/test/"+i)
     
 
 mode = 'train'
